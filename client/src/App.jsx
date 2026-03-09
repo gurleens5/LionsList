c23c8f874e881f0eb107f494ddca1d228c7c2298
Haivailable <97717295+Haivailable@users.noreply.github.com>
2026-03-09 18:31:35 -0400

    feat: US-01-T6,US-02-T5 --- signin,signup,backendconnection

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import ListingsPage from "./pages/ListingsPage";
 import ListingDetailsPage from "./pages/ListingDetailsPage";
+import api from "./lib/axios";
 
 function App() {
   const [page, setPage] = useState("home");
   const [selectedId, setSelectedId] = useState(null);
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState('');
+  
+  console.log(user);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const token = localStorage.getItem("token");
+      if (token) {
+        try {
+          const res = await api.get('/me', {
+            headers: {Authorization: `Bearer ${token}`}
+          })
+          setUser(res.data)
+        } catch (err) {
+          setError("Failed to fetch user data");
+          localStorage.removeItem("token");
+        }
+      }
+    };
+    fetchUser();
+  }, []);
 
   return (
     <div>
       {page === "home" && <Home setPage={setPage} />}
-      {page === "signin" && <SignIn setPage={setPage} />}
-      {page === "signup" && <SignUp setPage={setPage} />}
+      {page === "signin" && <SignIn setPage={setPage} setUser={setUser} />}
+      {page === "signup" && <SignUp setPage={setPage} setUser={setUser} />}
       {page === "listings" && <ListingsPage setPage={setPage} setSelectedId={setSelectedId} />}
       {page === "listing-details" && <ListingDetailsPage id={selectedId} setPage={setPage} />}
     </div>
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,9 +1,33 @@
 
 import Header from "../components/Header";
 import React, { useState } from "react";
+import api from "../lib/axios";
+import { useNavigate } from "react-router";
 
-function SignIn({ setPage }) {
-    const [password, setPassword] = useState("");
+function SignIn({ setPage, setUser }) {
+    const[formData, setFormData] = useState({
+        email: "",
+        password: "",
+    });
+    const [error, setError] = useState("");
+    const navigate = useNavigate();
+
+    const handleChange = (e) => {
+        setFormData({...formData, [e.target.name]: e.target.value})
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const res = await api.post("/login", formData);
+            localStorage.setItem("token", res.data.token);
+            console.log(res.data);
+            setUser(res.data);
+            navigate("/");
+        } catch (err) {
+            setError(err.response?.data?.message || "Login Failed")
+        }
+    }
 
     return (
         <div style={{ minHeight: "100vh", background: "#e6e4e4", fontFamily: "Georgia, sans-serif" }}>
@@ -23,21 +47,30 @@ function SignIn({ setPage }) {
                     <h2 style={{ margin: "0 0 0.3rem 0" }}>Sign In</h2>
                     <p style={{ color: "#888", margin: "0 0 2rem 0" }}>Use your YorkU email to continue</p>
 
+                    <form onSubmit={handleSubmit}>
+
                     <label>YorkU Email</label>
-                    <input type="email"
+                    <input 
+                        type="email"
+                        name="email"
+                        value={formData.email}
+                        onChange={handleChange}
                         placeholder="yourname@yorku.ca"
                         style={{ display: "block", width: "100%", padding: "0.8rem", margin: "0.4rem 0 1.2rem 0", borderRadius: "8px", border: "1px solid #ddd", boxSizing: "border-box", fontSize: "1rem", fontFamily: "Georgia, serif" }}/>
                     
                     <label>Password</label>
                     <input 
                         type="password"
-                        value= {password}
-                        onChange={e => setPassword(e.target.value)}
+                        name="password"
+                        value= {formData.password}
+                        onChange={handleChange}
                         style={{ display: "block", width: "100%", padding: "0.8rem", margin: "0.4rem 0 1.2rem 0", borderRadius: "8px", border: "1px solid #ddd", boxSizing: "border-box", fontSize: "1rem", fontFamily: "Georgia, serif" }}/>
                     
                     <button style={{ width: "100%", background: "#cc0000", color: "#fff", border: "none", borderRadius: "8px", padding: "0.9rem", fontSize: "1rem", fontWeight: "700", cursor: "pointer", fontFamily: "Georgia, serif" }}>
                         Sign In
                     </button>
+
+                    </form>
 
                     <p style={{ textAlign: "center", marginTop: "1.5rem", color: "#666" }}>
                         Don't have an account?{" "}
