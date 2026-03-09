e4d0cc21694c5490b29f0f48ddb9a53752d7b8d4
Haivailable <97717295+Haivailable@users.noreply.github.com>
2026-03-08 23:23:43 -0400

    feat: input verification and sanitization - username, email, password can be verified and are sanitzed

---
+++ server/controllers/authController.js
@@ -0,0 +1,75 @@
+import User from "../models/User"
+
+export async function register (req, res)  {
+    const username = req.body.username?.trim();
+    const email = req.body.email?.trim().toLowerCase();
+    const password = req.body.password?.trim();
+
+    try {
+        if (!username || !email || !password) {
+            return res.status(400).json({
+                message: "Please fill all required fields"
+            });
+        }
+
+        const emailExists = await User.findOne({email});
+        if (emailExists) {
+            return res.status(400).json({
+                message: "Email in use"
+            })
+        }
+
+        const userExists = await User.findOne({username});
+        if (userExists) {
+            return res.status(400).json({
+                message: "Username in use"
+            })
+        }
+
+        const user = await User.create({username, email, password});
+        res.status(201).json({
+            id: user._id,
+            usename: user.username,
+            email: user.email,        
+        })
+
+    } catch (err) {
+        res.status(500).json({
+            message: "Server error"
+        })
+    }
+}
+
+export async function login (req, res) {
+    const email = req.body.email?.trim().toLowerCase();
+    const password = req.body.password?.trim();
+
+    try {
+        if (!email || !password) {
+            return res.status(400).json({
+                message: "Please type your email or password"
+            });
+        }
+
+        const user = await User.findOne({email});
+
+        if (!user || !(await user.matchPassword(password))) {
+            return res.status(401).json({
+                message: "Invalid Email or Password"
+            });
+        }
+        res.status(200).json({
+            id: user._id,
+            usename: user.username,
+            email: user.email,    
+        });
+
+    } catch (err) {
+        res.status(500).json({
+            message: "Server error"
+        })
+    }
+}
+
+
+export default router;
