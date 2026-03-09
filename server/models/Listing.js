8785c94b80d71d291946dcdd26531523c7bce86f
Isha Chaudhry <ishachaudhry@iMacBook-Air.local>
2026-03-09 00:16:26 -0400

    US-08-T1 create listing schema

---
+++ server/models/Listing.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const listingSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    category: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0.01,
+    },
+    status: {
+      type: String,
+      default: "Available",
+      trim: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Listing = mongoose.model("Listing", listingSchema);
+
+export default Listing;
