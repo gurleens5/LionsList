718e5da1165af32e5e1d74318e6ea88555287429
Raymond Au <rayau29@my.yorku.ca>
2026-03-28 13:17:44 -0400

    US-22-T1: Implement backend route to fetch transactions for logged-in user

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import cors from "cors";
 import offersRoutes from "./routes/offers.routes.js";
+import transactionRoutes from "./routes/transactions.routes.js";
 import dns from "node:dns"
 
 dns.setServers(["1.1.1.1", "8.8.8.8"]);
@@ -20,6 +21,7 @@ app.use(cors());
 app.use("/api/listings", listingsRoutes);
 app.use("/api", authRoutes);
 app.use("/api/offers", offersRoutes);
+app.use("/api/transactions", transactionRoutes);
 
 app.get("/api/health", (req, res) => {
   res.json({ ok: true });
